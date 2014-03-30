2a0876004547b2052432fd6c22183c277dbbffae
Dave Guarino <dave@codeforamerica.org>
2014-03-30 12:32:27 -0700

    Add Voicemail.random method and test

--- spec/models/voicemail_spec.rb
+++ spec/models/voicemail_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Voicemail do
-  # pending
+  describe '#random' do
+    before do
+      voicemail_array = []
+      3.times { voicemail_array << create(:voicemail) }
+      @voicemail_ids = voicemail_array.map { |vm| vm.id }
+    end
+
+    it 'retrieves a valid voicemail' do
+      random_vm = Voicemail.random
+      expect(@voicemail_ids).to include(random_vm.id)
+    end
+  end
 end
